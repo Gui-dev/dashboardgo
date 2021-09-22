c0811f1750f3e13d5264600a64081033686927cb
Gui-dev <adrianon10@gmail.com>
2021-09-22 03:38:27 -0300

    feat: add google fonts Roboto

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,12 @@ class MyDocument extends Document {
   render () {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link rel="shortcut icon" href="/favicon.ico"/>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+        </Head>
         <body>
           <Main />
           <NextScript />
