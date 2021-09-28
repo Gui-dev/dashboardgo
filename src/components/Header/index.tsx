c39a764306ea6ee6d868bd32c41f08ecdad75b35
Gui-dev <adrianon10@gmail.com>
2021-09-28 03:30:53 -0300

    feat: add Header mobile responsiveness

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,14 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react'
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react'
 
 import { Profile } from './Profile'
 import { Notification } from './Notification'
 import { Logo } from './Logo'
 import { Search } from './Search'
+import { useSidebarDrawer } from 'hooks/useSidebarDrawer'
+import { RiMenuLine } from 'react-icons/ri'
 
 export const Header = () => {
+  const { onOpen } = useSidebarDrawer()
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
@@ -22,6 +25,16 @@ export const Header = () => {
       mt="4"
       px="6"
     >
+      { !isWideVersion && (
+        <IconButton
+          icon={ <Icon as={ RiMenuLine }/> }
+          fontSize="24"
+          mr="2"
+          variant="unstyled"
+          onClick={ onOpen }
+          aria-label="Open navigation"
+        />
+      ) }
       <Logo />
 
       { isWideVersion && (
