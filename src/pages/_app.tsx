29f0e254a63a380de9436d72e4313bcb1b7f4043
Gui-dev <adrianon10@gmail.com>
2021-10-01 19:12:20 -0300

    feat: add config React Query Devtools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 import { theme } from 'styles/theme'
 import { SidebarDrawerProvider } from 'context/SidebarDrawerContex'
@@ -20,6 +21,7 @@ const App = ({ Component, pageProps }: AppProps) => {
           <Component { ...pageProps }/>
         </SidebarDrawerProvider>
       </ChakraProvider>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   )
 }
